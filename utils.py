e03c3d82acff695f56a9a619235a9ad5af964536
Emilian Felecan <emil@springmerchant>
2013-01-21 14:59:28 +0200

    Return the theme dictionary and not the actual module.

--- utils.py
+++ utils.py
@@ -5,7 +5,8 @@ def get_themes():
     themes = []
     for importer, modname, is_pkg in pkgutil.iter_modules(__file__):
         if modname not in ['utils']:
-            themes.append(__import__(modname))
+            theme_module = __import__(modname)
+            themes.append(theme_module.theme)
 
     return themes
         
