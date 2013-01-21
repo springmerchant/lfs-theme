fec4666343704d2d5bb9c6d1ba8e04aee94eaaa1
Emilian Felecan <emil@springmerchant>
2013-01-21 16:30:11 +0200

    Use a simple list for all available designs.

--- utils.py
+++ utils.py
@@ -1,13 +1,5 @@
-import pkgutil
+# All available designs
+AVAILABLE_DESIGNS = ['lfs_theme.default', 'lfs_theme.realestate']
 
-# Discover all the installed themes
-def get_themes():
-    themes = []
-    for importer, modname, is_pkg in pkgutil.iter_modules(__file__):
-        if modname not in ['utils']:
-            theme_module = __import__(modname)
-            themes.append(theme_module.theme)
-
-    return themes
         
  
